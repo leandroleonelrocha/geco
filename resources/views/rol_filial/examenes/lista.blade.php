3e2617b5864ef475d08949640e730f133777ff72
Gabriel Donato <gdonato@agency.bitopia.com.ar>
2016-12-10 15:49:02 -0300

    Carga Inicial

--- resources/views/rol_filial/examenes/lista.blade.php
+++ resources/views/rol_filial/examenes/lista.blade.php
@@ -8,39 +8,29 @@
 				<div class="box-header">
 					<h3 class="box-title">@lang('examen.listadoacta')</h3>
 					<div class="box-tools pull-right no-print">
-
 						<a href="{{route('filial.examenes_nuevo')}}" class="btn btn-success text-white"> @lang('examen.nuevaacta')</a>
-						
 					</div>
 				</div>
 				@if(count($examenes) > 0)
 				<div class="box-body">
 					 <table id="example1" class="table table-bordered table-striped">
-
-						<thead> 
-						<tr>
-						<th>@lang('examen.numeroacta')</th>
-						<th>@lang('grupo.grupo')</th>
-						<th>@lang('grupo.docente')</th>
-					
-						<th></th>
-						
-						</tr> 
-						</thead>
+						<thead><tr>
+							<th>@lang('examen.numeroacta')</th>
+							<th>@lang('grupo.grupo')</th>
+							<th>@lang('grupo.docente')</th>
+							<th></th>
+						</tr></thead>
 						<tbody>
 						   @foreach($examenes as $examen)
 							    <tr role="row" class="odd">
 							        <td class="sorting_1">{{ $examen->nro_acta }}</td>
 						        	<td>@if(isset($examen->Grupo)) {{$examen->Grupo->fullname}} @endif</td>
 						        	<td>{{ $examen->Docente->fullname }}</td>
-						  			<td class="text-center">
-
-					           		<a href="{{route('filial.examenes_detalles',$examen->nro_acta)}}" title="@lang('lista.editar')"><i class="glyphicon glyphicon-primary-sign"></i></a>		
-								
+					           		<td class="text-center">
+					           		<a href="{{route('filial.examenes_detalles',$examen->nro_acta)}}" title="@lang('lista.editar')"><i class="btn btn-primary glyphicon glyphicon-pencil"></i></a>	
 					           		</td>
 							    </tr>
 						    @endforeach
-						
 				   		</tbody>
 				    </table>
         		</div><!-- Fin box-body -->
