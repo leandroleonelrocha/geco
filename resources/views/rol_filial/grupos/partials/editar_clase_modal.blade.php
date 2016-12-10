e1dd45f1a807dec7364a5bad1de33bc1aa82604b
leandroleonelrocha <rochaleandroleonel@gmail.com>
2016-12-10 18:17:02 -0300

    estilos de radio button

--- resources/views/rol_filial/grupos/partials/editar_clase_modal.blade.php
+++ resources/views/rol_filial/grupos/partials/editar_clase_modal.blade.php
@@ -13,9 +13,9 @@
 			  <div class="modal-body">
 				
 			  	<div class="form-group row">
-			      <label for="inputEmail3" class="col-sm-2 col-form-label">Personas inscriptas</label>
+			      <label for="inputEmail3" class="col-sm-2 col-form-label ">Personas inscriptas</label>
 			      <div class="col-sm-10">
-			      	<span> 21</span>
+					<span class="badge bg-green cantidad_personas"></span>
 				  </div>
 			    </div>
 
