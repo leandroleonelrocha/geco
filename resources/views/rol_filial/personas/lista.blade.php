6b5de3e4bbb388d3a549fb05ace69cadad1bcce2
unknown <llrocha@9DJ-1626.sds_domain_1.local>
2017-01-12 13:38:25 -0300

    pdf acta examen

--- resources/views/rol_filial/personas/lista.blade.php
+++ resources/views/rol_filial/personas/lista.blade.php
@@ -79,35 +79,6 @@
 
 @section('js')
 <script type="text/javascript">
-$(function() {
-
-    var start = moment().subtract(29, 'days');
-    var end = moment();
-
-    console.log(start);
-
-    function cb(start, end) {
-        $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-    }
-
-    $('#reportrange').daterangepicker({
-        startDate: start,
-        endDate: end,
-        ranges: {
-           'Today': [moment(), moment()],
-           'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-           'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-           'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-           'This Month': [moment().startOf('month'), moment().endOf('month')],
-           'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-        }
-    }, cb);
-
-    cb(start, end);
-    
-});
-</script>
-<script type="text/javascript">
 // $(".alert-dismissable").show();
 // $(".alert-dismissable").delay(3000).hide(600);
 
